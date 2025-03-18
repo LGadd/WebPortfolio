19a566b34f7ca826ad22bbe4f4ba8efc229e4a3c
LGadd <lbgadduk@gmail.com>
2025-03-18 00:16:45 +0000

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Form Submission Handling
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-  e.preventDefault();
-  alert('Thank you for your message!');
-  // You can add AJAX or backend integration here.
-});
